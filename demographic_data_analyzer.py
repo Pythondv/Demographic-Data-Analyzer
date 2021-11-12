48082e7c29e3b9b551a2945f23be0df155523b39
Yasi <yasaman.afshaar@gmail.com>
2021-11-12 14:15:15 +0100

    Remove Comment

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,9 +3,7 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = pd.read_csv('adult.data.csv')
-
-    # Somehting
+    df = pd.read_csv('adult.data.csv')    
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
   
